f46da110acd1454fa7f51779057a2fbe827a0768
san007chougule@gmail.com <san007chougule@gmail.com>
2022-06-08 09:03:25 +0530

    New spring boot webapp

---
+++ src/main/java/com/devops/webapp/DevopsWebappApplication.java
@@ -0,0 +1,13 @@
+package com.devops.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/webapp/DevopsWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
